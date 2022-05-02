91fe7c7a54faae3cac67c0f40058bb85c3a70157
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-02 11:38:29 -0400

    insert data into a table using sqlite in python

--- insert.py
+++ insert.py
@@ -9,4 +9,12 @@ conn = sqlite3.connect(":memory:")
 cursor = conn.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS books 
-                  (title TEXT, pages INTEGER)""")
+                  (title TEXT, 
+                  pages INTEGER)""")
+
+# insert data into database
+# use sqlite command: INSERT INTO tableName VALUES (columns you provided for the data base)
+cursor.execute("INSERT INTO books VALUES ('Going To Meet The Man?', 15)")
+
+# used conn obj to insert data to the table
+conn.commit()
