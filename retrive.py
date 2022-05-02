fa86925eec730816c946ac5e459bc4ecd9784aa3
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-02 17:56:02 -0400

    retrieve data from a table using sqlite in python

--- retrive.py
+++ retrive.py
@@ -8,7 +8,29 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS books
                   (title TEXT, 
                   pages INTEGER)""")
 
-cursor.execute("INSERT INTO books VALUES ('Going To Meet The Man?', 15)")
-
+# another way to insert values into your column is to create a list of tuples
+books = [
+    ("Going to Meet the Man", 15),
+    ("Are You My Mother", 23),
+    ("The Giver", 233),
+]
+# insert table
+# (?, ?): subs for the book name and the book page
+cursor.executemany("INSERT INTO books VALUES (?, ?)", books)
 conn.commit()
 
+
+# retrieve all data
+cursor.execute('SELECT * FROM books')
+
+
+# retrieve specified data
+# cursor.execute('SELECT * FROM books WHERE title="Going to Meet the Man"')
+
+
+# fetchone(): gets the data from one row of table column, get a tuple
+# fetchall(): gets all rows from the table, get a list of tuples
+data = cursor.fetchall()
+
+# prints a tuple
+print(data)
