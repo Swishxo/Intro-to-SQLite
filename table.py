01d4a0ab1b14d06b532f57d4a61df8ad7a4570e9
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-02 08:33:19 -0400

    create a table using sqlite in python

--- table.py
+++ table.py
@@ -1,9 +1,10 @@
 """
 execute(): allow you to store data directly to the database
-sql commands: CREATE TABLE, TEXT, INTEGER
+sql commands: CREATE TABLE, TEXT, INTEGER, IF NOT EXISTS
     CREATE TABLE: is a command used to make a database and the name of the database is lowercase
-    TEXT: is the data-type of a column that stores a specific data
-
+    TEXT: is the data-type of a column that stores a specific data ex:(nameOfColumn TEXT)
+    INTEGER: is the data-type of a column that stores a specific data ex:(nameOfColumn INTEGER)
+    IF NOT EXISTS: is command used to find out if your database is already created
 """
 import sqlite3
 
@@ -12,4 +13,6 @@ conn = sqlite3.connect(":memory:")
 cursor = conn.cursor()
 
 # Create the table
-cursor.execute("CREATE TABLE books (title TEXT, pages INTEGER)")
+# """: allow for the syntax to be on multiple lines making it readable
+cursor.execute("""CREATE TABLE IF NOT EXISTS books 
+                  (title TEXT, pages INTEGER)""")
