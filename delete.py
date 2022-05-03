7872a9f4a2bbb590d1bdfd68f7671fa6db1ef0d3
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-02 20:55:21 -0400

    delete data from a table using sqlite in python

--- delete.py
+++ delete.py
@@ -8,7 +8,6 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS books
                   (title TEXT, 
                   pages INTEGER)""")
 
-
 books = [
     ("Going to Meet the Man", 15),
     ("Are You My Mother", 23),
@@ -20,8 +19,19 @@ conn.commit()
 
 cursor.execute('SELECT * FROM books')
 
+data = cursor.fetchall()
+print("Previous Table: ")
+print(data)
 
+# delete data from  table
+cursor.execute("DELETE FROM books WHERE title='Are You My Mother'")
+# commit changes to table book
+conn.commit()
+
+# select updated table book
+cursor.execute("SELECT * FROM books")
+# retrieve data from table
 data = cursor.fetchall()
 
-
+print("UPDATED TABLE: ")
 print(data)
