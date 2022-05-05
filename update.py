64ea95be332c1c56f5ed6d3da68e8d1f97715182
Stephon Tidd <stidd@mail.valenciacollege.edu>
2022-05-04 21:47:02 -0400

    update data from a table using sqlite in python

--- update.py
+++ update.py
@@ -1,4 +1,37 @@
 """
 
 """
-print("Hello")
+import sqlite3
+
+conn = sqlite3.connect(":memory:")
+
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS books 
+                  (title TEXT, 
+                  pages INTEGER)""")
+
+books = [
+    ("Going to Meet the Man", 15),
+    ("Are You My Mother", 23),
+    ("The Giver", 233),
+]
+
+cursor.executemany("INSERT INTO books VALUES (?, ?)", books)
+conn.commit()
+
+cursor.execute('SELECT * FROM books')
+
+data = cursor.fetchall()
+print("Previous Table: ")
+print(data)
+
+cursor.execute("DELETE FROM books WHERE title='Are You My Mother'")
+
+conn.commit()
+
+cursor.execute("SELECT * FROM books")
+data = cursor.fetchall()
+
+print("UPDATED TABLE: ")
+print(data)
